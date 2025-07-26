b85329a5f66cf120a85b257d8e03907ee7a4d9c2
Dhyan V Shetty <dhyanshetty777@gmail.com>
2025-07-26 18:26:19 +0530

    Final fixes before redeploy

--- anuser/urls.py
+++ anuser/urls.py
@@ -5,8 +5,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('all/',Animeview.as_view() ),
     path('anime/<int:pk>/',AnimeDetail.as_view() ),
-    path("/up/<int:pk>/",Animeupdate.as_view()),
-    path("/del/<int:pk>/",AnimeDeleteView.as_view()),
+    path("up/<int:pk>/",Animeupdate.as_view()),
+    path("del/<int:pk>/",AnimeDeleteView.as_view()),
     path('post/',Animepost.as_view()),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
